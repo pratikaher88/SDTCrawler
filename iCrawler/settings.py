d686dab8e1f6f76c939c26cba296f46bead01cd8
pratikaher <pratikaher88@gmail.com>
2019-12-08 19:22:39 +0530

    make it better

--- iCrawler/settings.py
+++ iCrawler/settings.py
@@ -128,11 +128,11 @@ STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 # STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
