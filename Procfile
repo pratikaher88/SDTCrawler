02aa4bc86cfaf3acf38ab21819169a0a96ebd509
pratikaher <pratikaher88@gmail.com>
2019-12-08 19:11:46 +0530

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn iCrawler.wsgi --log-file -
--- iCrawler/settings.py
+++ iCrawler/settings.py
@@ -123,3 +123,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
