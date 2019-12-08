4029e97dfc0c28775c6db36acb9a1f20acd9fa14
pratikaher <pratikaher88@gmail.com>
2019-12-08 19:15:40 +0530

    make it better

--- iCrawler/settings.py
+++ iCrawler/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0qqskd*@bsrt1=$&*$gd9zje49vmeb_k6(yr*%m69ha$*72z(p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -134,5 +134,5 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
